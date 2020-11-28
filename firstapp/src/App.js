41671fc9e3549ad74b844a8b912ce4de5d7a003e
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-11-28 10:19:18 +0000

    first commit, with something static created in REACT

---
+++ firstapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tweet from './tweet';
+
+function App() {
+
+return(
+  <div className="app">
+  <Tweet />
+  <Tweet />
+  </div> //jsx
+
+);
+}
+
+export default App;
---
+++ firstapp/src/tweet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+function Tweet() {
+  return (
+    <div className="tweet">
+    <h3> Name</h3>
+    <p> It's a random tweet</p>
+    <h3> Number of likes </h3>
+    </div>
+  );
+}
+
+export default Tweet;
