9588785e829d84dd8a33c50303d5b98c8ec15966
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-11-28 11:04:02 +0000

    use of useState and definition of some data to outmaticslly render within predefined component

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,22 +1,17 @@
 import React, {useState} from 'react';
 import './App.css';
+import Tweet from './tweet'
 //making dynamic featurs using useState
 function App() {
-const [isRed, setRed] = useState(false);
-const [count, setCount] = useState(0);
-//function to change the count
-const increment =() => {
-  setCount(count + 1);
-}
-//function to change the colour of the count
-const colour = () =>{
-  setRed(!isRed);
-}
+//Imgaine you get this by fetching some API, that's great,
+const [users, setUser] = useState([
+  {name: "Lucky Joe", message: "Hi fellas!", likes: "1200"},
+  {name: "Unlucky Doe", message: "Hello people.", likes:"-1"}
+])
 return(
+  //you get to automatically map data from fetch to your predefined react component
   <div className="app">
-  <button onClick={increment}>Increment</button>
-  <button onClick={colour}>Change colour</button>
-  <h1 className={isRed ? "red" : ""}>{count}</h1>
+  {users.map(user => <Tweet name={user.name} message={user.message} likes={user.likes} />)}
   </div> //jsx
 
 );
